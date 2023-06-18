12d8c4df9bae1c4d41e34fac56b93dc79fa9df90
Hanna-dkr <135759368+Hanna-dkr@users.noreply.github.com>
2023-06-18 18:00:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+// Notes:Data types: Strings (var text1 = "js wrapped in double quotes")//
+// var text1 = "He said \"Hello\"." //
+// Numbers: var numberInteger = "2"; var numberFloat = "2.2"//
+// Booleans: their values are either true or false. var boolean1 = true; var boolean2 = false //
+// Null and undefined: They both indicate no value has been set. undefined means never set, null means empty value //
+// Objects are complex data types.
+
+
+
+
 console.log("Hello and welcome to my portfolio site")

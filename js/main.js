997570f74d71d950573006d6ebed57b20abbb8f2
Hanna-dkr <135759368+Hanna-dkr@users.noreply.github.com>
2023-06-19 20:41:11 +0200

    Linked JQuery-3.3.1.min.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Numbers: var numberInteger = "2"; var numberFloat = "2.2"//
 // Booleans: their values are either true or false. var boolean1 = true; var boolean2 = false //
 // Null and undefined: They both indicate no value has been set. undefined means never set, null means empty value //
-// Objects are complex data types.
+// Objects are complex data types. var person = { name: "John", age: 39};
 
 
 
